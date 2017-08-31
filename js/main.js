5c25180bf65e78c992c4c28ce5fff2727af4fef2
Patriquin <denise.patriquin@libertymutual.com>
2017-08-30 22:05:57 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+// console.log(cardsInPlay); // not sure if I need to use an index or not
+console.log(cardsInPlay);
+console.log("User flipped" + " " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped" + " " + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert('Sorry, try again.');
+	}
+}
