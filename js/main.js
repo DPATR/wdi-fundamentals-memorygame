9f4f41d6a98626d233d37c0499802c071b1aaf60
Patriquin <denise.patriquin@libertymutual.com>
2017-08-31 15:03:43 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 console.log("Up and running!");
+
+//Arrays below have Global scope
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-// console.log(cardsInPlay); // not sure if I need to use an index or not
-console.log(cardsInPlay);
-console.log("User flipped" + " " + cardOne);
+
+checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log('Sorry, try again.');
+		}
+}
+
+var flipCard = function(cardID) {
+	console.log(cardsInPlay);
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cardID);
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert('Sorry, try again.');
+	}
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+/*console.log("User flipped" + " " + cardOne);
 var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped" + " " + cardTwo);
@@ -15,4 +39,4 @@ if (cardsInPlay.length === 2) {
 	} else {
 		alert('Sorry, try again.');
 	}
-}
+}*/
